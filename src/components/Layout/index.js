76019910fe38219bcedf4291b00c33b90adfcb9c
Naomi Parte <83632689+Naomi-Parte@users.noreply.github.com>
2023-09-16 12:42:53 +1200

    base code from youtube

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import './index.scss';
+import Sidebar from '../Sidebar'
+
+const Layout = () => {
+    return (
+        <div classname="App">
+            <Sidebar/>
+            <div className='page'>
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default Layout;
